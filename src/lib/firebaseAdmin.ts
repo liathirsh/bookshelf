a2fa85cd32b8f3f212b861a97a258effeed437e3
Liat Hirsh <liat@urbanpixels.com>
2025-02-18 21:35:43 -0500

    fixing deployment issues

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
     const serviceAccount = {
-        projectId: process.env.FIREBASE_PROJECT_ID,
+        project_id: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
     };
